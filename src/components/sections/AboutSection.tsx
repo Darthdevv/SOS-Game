eb9c2f2b8c46e97e225d8f40adcb18a09106f309
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2025-06-16 02:40:08 +0300

    🧟‍♂️  feat: improving performance

--- src/components/sections/AboutSection.tsx
+++ src/components/sections/AboutSection.tsx
@@ -1,24 +1,16 @@
 "use client";
-import Image from "next/image";
 import { Carousel } from "../Carousel";
 
 const AboutSection = () => {
   return (
     <section
       id="about"
-      className="relative min-h-screen flex items-center justify-center text-white overflow-hidden"
+      className="min-h-screen bg-cover bg-center text-white flex flex-col items-center justify-center"
+      style={{ backgroundImage: "url('/images/bg image 3.jpg')" }}
     >
-      {/* Background Image */}
-      <Image
-        src="/images/bg image 3.jpg"
-        alt="SOS Game Hero Background"
-        fill
-        className="object-cover"
-        priority
-      />
 
       {/* Content */}
-      <div className="relative z-20 w-full">
+      <div className="relative z-20 w-full flex items-center justify-center">
         <Carousel />
       </div>
     </section>
